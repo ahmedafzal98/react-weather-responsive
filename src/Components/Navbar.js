606b19c1d1247a5d56cedae9cfd0e909e4ca0d17
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2022-09-26 23:20:37 +0500

    autocomplete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [countries, setCountries] = useState([]);
+  // const [suggestions, setSuggestions] = useState([]);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  useEffect(() => {
+    //  getCountryData();
+  }, []);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${event.target.value}&limit=5&appid=d4d04688a9d2f3d90f7b83e0b39ac6f4`
+      )
+      .then((res) => {
+        setCountries(res.data);
+        // setSuggestions(res.data);
+        // console.log(countries);
+      });
+  };
   return (
     <>
       <nav className="navbar">
+        {countries.map((country) => {
+          console.log(JSON.stringify(country));
+          <h1>{country.name}</h1>;
+        })}
         <ul>
           <li className="navbar-logo">
             <Link to="/">
@@ -29,16 +52,27 @@ function Navbar() {
               <input
                 type="text"
                 name=""
+                onChange={handleChange}
                 id="input-text"
                 placeholder="Weather In Your City"
               />
+              {countries &&
+                countries.map((item, index) => {
+                  console.log("test", item);
+                  return (
+                    <div key={index}>
+                      <ul className="suggestions" >
+                        <li>{item.name}</li>
+                      </ul>
+                    </div>
+                  );
+                })}
             </form>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Guide
               </Link>
             </li>
-
             <li className="nav-item">
               <Link
                 to="/services"
