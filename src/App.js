8806f099f897e522b9c7665b9bb4aab1c1a7a23d
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2022-10-13 20:38:25 +0500

    s

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Home from "./Components/Pages/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import WeatherContext from "./Contexts/WeatherContext";
 
 function App() {
-  const [weather, setWeather] = useState();
-  const [hourlyWeather, setHourlyWeather] = useState();
+  const [weather , setWeather] = useState();
+  const [hourlyWeather , setHourlyWeather] = useState();
   const dispatchUserEvent = (payLoad) => {
-    setWeather(payLoad.data);
-    setHourlyWeather(payLoad.hourlyForecast);
-    console.log("Ahmed", weather);
+    
+    setWeather(payLoad.data)
+    setHourlyWeather(payLoad.hourlyForecast)
+    console.log("Ahmed" , weather);
   };
   return (
     <>
       <Router>
-        <WeatherContext.Provider
-          value={{ hourlyWeather, weather, dispatchUserEvent }}
-        >
+        <WeatherContext.Provider value={{hourlyWeather,weather,dispatchUserEvent}}>
+          <Navbar />
           <Routes>
-            <Navbar />
-            <Route path="/react-weather-responsive" exact element={<Home />} />
+            <Route path="/" exact element={<Home />} />
           </Routes>
         </WeatherContext.Provider>
       </Router>
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../Navbar";
 import Banner from "../Banner";
 import MainSection from "../MainSection";
 import Slider from "../Slider";
